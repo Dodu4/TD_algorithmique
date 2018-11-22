cb53ff8150ed6a7597aade4689e6b0af97782e2b
Dodu4 <45232997+Dodu4@users.noreply.github.com>
2018-11-22 09:06:21 +0100

    Update MIB_exos.py

--- MIB_exos.py
+++ MIB_exos.py
@@ -17,3 +17,23 @@ print("3. La réponse est ",Triples)
 ### Question 4 : quel est l'ensemble des couples (alien,allée) pour chaque alien:
 Couples= {(Alien.Nom, Alien.NoCabine for NoAllee in Cabine) for Alien in BaseAliens}
 print("4. La réponse est ",Couples)
+
+### Question 5 : quel est l'ensemble de tous les aliens de l'allée 2 ?
+
+### Question 6 : quel est l'ensemble de toutes les planètes dont sont originaires les aliens habitant une cellule de numéro pair ?
+
+### Question 7 : quel est l'ensemble des aliens dont les gardiens sont originaires de Terminus ?
+
+### Question 8 : quel est l'ensemble des gardiens des aliens féminins qui mangent du bortsch ?
+
+### Question 9 : quel est l'ensemble des cabines dont les gardiens sont originaires de Terminus ou dont les aliens sont des filles ?
+
+### Question 10 : Y a-t-il des aliments qui commencent par la même lettre que le nom du gardien qui surveille l'alien qui les mange ?
+
+### Question 11 : Y a-t-il cdes aliens originaires d'Euterpe ?
+
+### Question 12 : Est-ce que tous les aliens ont un 'x' dans leur nom ?
+
+### Question 13 : Est-ce que tous les aliens qui ont un 'x' dans leur nom ont un gardien qui vient de Terminus ?
+
+### Question 14 : Existe-t-il un alien masculin originaire de Trantor qui mange du Bortsch ou dont le gardien vient de Terminus ?
